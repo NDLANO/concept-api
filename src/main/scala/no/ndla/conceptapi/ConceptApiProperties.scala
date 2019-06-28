2300790d0166f36ac1bf4665b1522aa8e3d62c9d
Bartek <bartosz.tracz@knowit.no>
2019-06-28 15:20:47 +0200

    deletes unused file and unused api dependencies

--- src/main/scala/no/ndla/conceptapi/ConceptApiProperties.scala
+++ src/main/scala/no/ndla/conceptapi/ConceptApiProperties.scala
@@ -11,7 +11,6 @@ import com.typesafe.scalalogging.LazyLogging
 import no.ndla.network.secrets.PropertyKeys
 import no.ndla.network.secrets.Secrets.readSecrets
 import no.ndla.network.{AuthUser, Domains}
-import no.ndla.validation.ResourceType
 
 import scala.util.Properties._
 import scala.util.{Failure, Success}
@@ -38,19 +37,6 @@ object ConceptApiProperties extends LazyLogging {
 
   val resourceHtmlEmbedTag = "embed"
   val ApiClientsCacheAgeInMs: Long = 1000 * 60 * 60 // 1 hour caching
-
-  val ArticleApiHost: String =
-    propOrElse("ARTICLE_API_HOST", "article-api.ndla-local")
-  val LearningpathApiHost: String =
-    propOrElse("LEARNINGPATH_API_HOST", "learningpath-api.ndla-local")
-  val AudioApiHost: String =
-    propOrElse("AUDIO_API_HOST", "audio-api.ndla-local")
-  val DraftApiHost: String =
-    propOrElse("DRAFT_API_HOST", "draft-api.ndla-local")
-  val ImageApiHost: String =
-    propOrElse("IMAGE_API_HOST", "image-api.ndla-local")
-  val SearchApiHost: String =
-    propOrElse("SEARCH_API_HOST", "search-api.ndla-local")
 
   val SearchServer: String =
     propOrElse("SEARCH_SERVER", "http://search-concept-api.ndla-local")
