fb8a5689e9995f4f5719bea73ee1525557962f91
N4G1 <bartezzzzz@hotmail.com>
2019-07-24 13:00:06 +0200

    typos in documentation

--- src/main/scala/no/ndla/conceptapi/model/api/Copyright.scala
+++ src/main/scala/no/ndla/conceptapi/model/api/Copyright.scala
@@ -15,8 +15,8 @@ import scala.annotation.meta.field
 
 @ApiModel(description = "Description of copyright information")
 case class Copyright(
-    @(ApiModelProperty @field)(description = "Describes the license of the article") license: Option[License],
-    @(ApiModelProperty @field)(description = "Reference to where the article is procured") origin: Option[String],
+    @(ApiModelProperty @field)(description = "Describes the license of the concept") license: Option[License],
+    @(ApiModelProperty @field)(description = "Reference to where the concept is procured") origin: Option[String],
     @(ApiModelProperty @field)(description = "List of creators") creators: Seq[Author],
     @(ApiModelProperty @field)(description = "List of processors") processors: Seq[Author],
     @(ApiModelProperty @field)(description = "List of rightsholders") rightsholders: Seq[Author],
