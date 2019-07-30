7280a4339de325cfdc00c9467695c2e113e9132c
katrinewi <katrinew0702@gmail.com>
2019-07-30 12:57:41 +0200

    Changes requested on PR

--- src/main/scala/no/ndla/conceptapi/model/api/ConceptMetaImage.scala
+++ src/main/scala/no/ndla/conceptapi/model/api/ConceptMetaImage.scala
@@ -10,9 +10,9 @@ import org.scalatra.swagger.annotations.{ApiModel, ApiModelProperty}
 
 import scala.annotation.meta.field
 
-@ApiModel(description = "Meta description of the concept")
+@ApiModel(description = "Meta image for the concept")
 case class ConceptMetaImage(
     @(ApiModelProperty @field)(description = "The meta image url") url: String,
     @(ApiModelProperty @field)(description = "The alt text for the meta image") alt: String,
     @(ApiModelProperty @field)(description =
-      "The ISO 639-1 language code describing which article translation this meta description belongs to") language: String)
+      "The ISO 639-1 language code describing which concept translation this meta description belongs to") language: String)
