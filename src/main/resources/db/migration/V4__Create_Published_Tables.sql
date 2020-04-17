4ec2fd5eaf3ce70e3c13118e2ecedc78b29251a3
Jonas Natten <jonas.natten@knowit.no>
2020-04-17 06:22:27 +0200

    Update src/main/resources/db/migration/V4__Create_Published_Tables.sql
    Adds IF NOT EXISTS to new column migration
    
    Co-Authored-By: Gunnar Velle <gunnar.velle@gmail.com>

--- src/main/resources/db/migration/V4__Create_Published_Tables.sql
+++ src/main/resources/db/migration/V4__Create_Published_Tables.sql
@@ -1,4 +1,4 @@
-ALTER TABLE conceptdata ADD COLUMN revision integer not null default 1;
+ALTER TABLE conceptdata ADD COLUMN IF NOT EXISTS revision integer not null default 1;
 
 -- Clones conceptdata into publishedconceptdata
 CREATE TABLE publishedconceptdata as (select * from conceptdata);
