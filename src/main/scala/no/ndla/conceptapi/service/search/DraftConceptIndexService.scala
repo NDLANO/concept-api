1e319397ccd5e424a55c96b7299bda165d708c27
Gunnar Velle <gunnar.velle@gmail.com>
2020-11-27 14:10:44 +0100

    Forgot to change draft-field

--- src/main/scala/no/ndla/conceptapi/service/search/DraftConceptIndexService.scala
+++ src/main/scala/no/ndla/conceptapi/service/search/DraftConceptIndexService.scala
@@ -46,7 +46,7 @@ trait DraftConceptIndexService {
       mapping(documentType).fields(
         List(
           intField("id"),
-          keywordField("defaultTitle").fields(keywordField("lower").normalizer("lower")),
+          keywordField("defaultTitle").normalizer("lower"),
           keywordField("subjectIds"),
           nestedField("metaImage").fields(
             keywordField("imageId"),
