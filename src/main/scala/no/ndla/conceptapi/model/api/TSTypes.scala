50c6d2d88892b63bc8b14ca1d22fc7c0ece07c17
N4G1 <bartezzzzz@hotmail.com>
2021-11-09 12:30:26 +0100

    removed unnecessary comment

--- src/main/scala/no/ndla/conceptapi/model/api/TSTypes.scala
+++ src/main/scala/no/ndla/conceptapi/model/api/TSTypes.scala
@@ -3,11 +3,6 @@ package no.ndla.conceptapi.model.api
 import com.scalatsi.TypescriptType.TSNull
 import com.scalatsi._
 
-/**
-  * The `scala-tsi` plugin is not always able to derive the types that are used in `Seq` or other generic types.
-  * Therefore we need to explicitly load the case classes here.
-  * This is only necessary if the `sbt generateTypescript` script fails.
-  */
 object TSTypes {
   // This alias is required since scala-tsi doesn't understand that Null is `null`
   // See: https://github.com/scala-tsi/scala-tsi/issues/172
